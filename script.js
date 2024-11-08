6811dd35d8da6cad8b08c6db9f7dd5b3b06708df
niks1503 <niksharma1503@gmail.com>
2024-11-08 13:00:45 +0545

    am-pm updated

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const hours = document.querySelector(".hours_para");
 const minutes = document.querySelector(".minutes_para");
 const seconds = document.querySelector(".seconds_para");
+const am_pm = document.querySelector(".am-pm");
 
 function updateClock() {
   let h = new Date().getHours();
-  hours.innerText = h;
   let m = new Date().getMinutes();
-  minutes.innerText = m;
   let s = new Date().getSeconds();
+
+  if (h > 12) {
+    h = h - 12;
+    am_pm.innerText = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hours.innerText = h;
+  minutes.innerText = m;
   seconds.innerText = s;
 
   setTimeout(() => {
